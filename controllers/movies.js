b8f4e17d400de2533bd0ee2c95224bb54a01cf2c
Inna Gosteva <innagosteva31@yandex.ru>
2023-02-28 16:53:52 +0500

    2st

--- controllers/movies.js
+++ controllers/movies.js
@@ -50,7 +50,7 @@ module.exports.addMovie = (req, res, next) => {
 };
 
 module.exports.deleteMovie = (req, res, next) => {
-  movieSchema.findById(req.params._id)
+  movieSchema.findById(req.params.id)
     .orFail(() => {
       throw new NotFoundErr('Фильм не найден');
     })
